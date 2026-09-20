cf8023cbf549484db8785f539189e898bf968627
NIhal-Gowda <nihalgowda0102@gmail.com>
2026-09-20 12:17:58 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (80.39%) - LeetSync

---
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def smallerNumbersThanCurrent(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        sorted_nums=sorted(nums)
+        smaller_count={}
+        for i,num in enumerate(sorted_nums):
+            if num not in smaller_count:
+                smaller_count[num]=i
+        return[smaller_count[num] for num in nums]
